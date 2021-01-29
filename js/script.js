f791ca4e62c320dc1578adab7b465dc96af7e8eb
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-01-29 13:59:35 +0000

    Added comments in script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function getNumberInfo() {
     // Get the value of the input field
     let num = document.getElementById("mynumber").value;
     // Check if number is valid
-    if (isNaN(num) || num<0 || num>9999999999 ) {
+    if (isNaN(num) || num<0 || num>9999999999 || !Number.isInteger(Number(num))) {
        txt += `Invalid Input.  Please enter a number between 0 and 9999999999.`;
     } else {
         // Display entered number
@@ -20,16 +20,20 @@ function getNumberInfo() {
      document.getElementById("numinfo").innerHTML = txt;
 }
 
+// Function to Check if the number is a palindrome
 function isPalindrome(x) {
     let answer = 0;
     let temp = x;
+    // Use While Loop
     while (temp > 0) {
         answer = (10*answer) + (temp%10);
         temp = Math.floor(temp/10);
     }
+    // Compare the answer with the entered value
     return answer == x;
 }
 
+// Function to clear the information
 function clearInfo() {
     let txt="";
     document.getElementById("numinfo").innerHTML = txt;
