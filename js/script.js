4d9a42d1ee1d5e5742bb09c02a116947d8cf2198
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-01-29 13:33:32 +0000

    Added function isPalindrome in script.js

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Set Up Variables
+let txt="";
+function getNumberInfo() {
+    // Get the value of the input field
+    let num = document.getElementById("mynumber").value;
+    // Check if number is valid
+    if (isNaN(num) || num<0 || num>9999999999 ) {
+        alert("Invalid input.  Must be a number and it is between 0 and 9999999999.");
+    } else {
+        // Display entered number
+        txt += `You have entered the number {num}.<p>`;
+        // Check if the number is a palidrome
+        if (isPalindrome(num)) {
+            txt += `Number {num} is a palindrome`;
+        } else {
+            txt += `Number {num} is not a palindrome`;
+        }
+        document.getElementById("numinfo").innerHTML = txt;
+    }
+}
+
+function isPalindrome(x) {
+    let answer = 0;
+    let temp = x;
+    while (temp > 0) {
+        answer = (10*answer) + (temp%10);
+        temp = Math.floor(temp/10);
+    }
+    return answer == x;
+}
